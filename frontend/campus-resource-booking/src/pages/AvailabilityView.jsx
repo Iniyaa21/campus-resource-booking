242cd69956b729f170fc3236b9d6f8bde68c5209
Bharath Aashish <bharathaasish@gmail.com>
2026-02-09 10:01:31 +0530

    Frontend: routing

---
+++ frontend/campus-resource-booking/src/pages/AvailabilityView.jsx
@@ -0,0 +1,10 @@
+function AvailabilityView() {
+  return (
+    <div>
+      <h1>Availability View</h1>
+    </div>
+  );
+}
+
+export default AvailabilityView;
+
---
+++ frontend/campus-resource-booking/src/pages/MyBookings.jsx
@@ -0,0 +1,10 @@
+function MyBookings() {
+  return (
+    <div>
+      <h1>My Bookings</h1>
+    </div>
+  );
+}
+
+export default MyBookings;
+
